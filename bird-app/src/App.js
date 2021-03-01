676872e57bff1fb933169b104327a6f92fa5a74d
MarkStuebs <markmewords@hotmail.com>
2021-03-01 07:44:16 -0800

    boutme

--- bird-app/src/App.js
+++ bird-app/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Link, Switch, Redirect } from "react-router-dom";
-//import Homepage from "./Homepage";
-//import Show from "./Show";
+import {    BrowserRouter as Router   } from "react-router-dom";
 import axios from 'axios';
 import Search from './Search';
+import AboutMe from './AboutMe/AboutMe';
 
 
 
@@ -21,7 +21,7 @@ class App extends Component {
       //        }
     }
     let response = axios.get('https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=Nx5vC1gM25R5WZl5kR7p0V3M7Ry2TXXubzAkG1bQals')
-       .then(response => {
+      .then(response => {
         console.log(response)
         this.setState({
           plants: response.data
@@ -38,20 +38,31 @@ class App extends Component {
     return (
       <div>
         <main>
+
           <Link to='/'>
             <h1>Plants List</h1>
           </Link>
+          <Link to='/aboutme/'>
+            <h1>About Us</h1>
+          </Link>
+
           <Switch>
+          
+          <Router>
+            <Route exact path='/aboutme/' component={AboutMe} />
+          
+          </Router>
             <Route to='/search/'
               render={(props) =>
               (<Search {...this.state} />
               )}
             />
+            
           </Switch>
 
-        </main>
+        </main >
 
-      </div>
+      </div >
     );
   }
 }
